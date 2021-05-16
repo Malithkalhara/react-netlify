d3848c3d08586b91253225358078e823741b9159
Malith Kalhara <malith.kalhara@randoli.ca>
2021-05-16 19:56:17 +0530

    Added auth0 authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import React from "react";
+import { Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+import Home from "./components/home";
+import ProtectedRoute from "./auth/protected-route";
+
+import "./App.css";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        CI/CD works fine with netlify
-      </header>
+    <div id="app" className="d-flex flex-column h-100">
+      <div className="container flex-grow-1">
+        <Switch>
+          <ProtectedRoute path="/" exact component={Home} />
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
